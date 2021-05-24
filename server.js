caea0e574252850e16dd89d8e50f9e00e01c99fc
Ajil Abdalqader <ajilqado@gmail.com>
2021-05-24 22:07:27 +0200

    in-create_Username-connectedWith-socket-if-user-exist-theUserNameAlreadyTaken-if_not_make-New_UserName-&&-takeNewUserName-from_array-&&-in-Index.html-Create-UserName-form

--- server.js
+++ server.js
@@ -13,10 +13,36 @@ app.get('/', function(req, res){
 //connection to socket
 io.sockets.on('connection', function(socket){
 	console.log('Socket Connected...');
+    usernames = [];
 
+	socket.on('new user', function(data, callback){
+		
+        if(usernames.indexOf(data) != -1){
+			callback(false);
+		} else {
+			callback(true);
+			socket.username = data;
+			usernames.push(socket.username);
+			updateUsernames();
+		}
+	});
+
+    // Update Usernames
+	function updateUsernames(){
+		io.sockets.emit('usernames', usernames);
+	}
 	// Send Message
 	socket.on('send message', function(data){
 		io.sockets.emit('new message', {msg: data, user:socket.username});
 	});
    
+		// Disconnect
+	socket.on('disconnect', function(data){
+		if(!socket.username){
+			return;
+		}
+        
+		usernames.splice(usernames.indexOf(socket.username), 1);
+		updateUsernames();
+	});
 })
